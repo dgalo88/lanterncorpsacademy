655246cac9c7da0e48cd53d290c1c9901c0bcfad
knighthash@gmail.com <knighthash@gmail.com@6532114f-c851-e024-fd2a-a63d0af6f600>
2010-04-27 04:06:50 +0000

    IS panel registro2 experiments III

--- trunk/lanternSVNtest/src/com/ulasoft/lanterncorpsacademy/paneles/PanelRegistro2.java
+++ trunk/lanternSVNtest/src/com/ulasoft/lanterncorpsacademy/paneles/PanelRegistro2.java
@@ -1,24 +1,22 @@
 package com.ulasoft.lanterncorpsacademy.paneles;
 
-import com.ulasoft.lanterncorpsacademy.GUIStyles;
+import nextapp.echo.app.Border;
 import nextapp.echo.app.Button;
 import nextapp.echo.app.Color;
 import nextapp.echo.app.Column;
 import nextapp.echo.app.ContentPane;
 import nextapp.echo.app.Extent;
-import nextapp.echo.app.Grid;
 import nextapp.echo.app.Insets;
 import nextapp.echo.app.Label;
 import nextapp.echo.app.Panel;
-import nextapp.echo.app.PasswordField;
 import nextapp.echo.app.RadioButton;
 import nextapp.echo.app.Row;
-import nextapp.echo.app.TextField;
-import nextapp.echo.app.button.ButtonGroup;
 import nextapp.echo.app.event.ActionEvent;
 import nextapp.echo.app.event.ActionListener;
 import nextapp.echo.extras.app.AccordionPane;
 import nextapp.echo.extras.app.layout.AccordionPaneLayoutData;
+
+import com.ulasoft.lanterncorpsacademy.GUIStyles;
 
 public class PanelRegistro2 extends Panel {
 
@@ -58,7 +56,10 @@ public class PanelRegistro2 extends Panel {
 		acc.setTitle("Verde - Green Lantern Corps");
 		ContentPane cp = new ContentPane();
 		cp.setLayoutData(acc);
-		cp.add(btnRadioVerde);
+		Column radiocol = new Column(); radiocol.setCellSpacing(new Extent(250));
+		radiocol.add(btnRadioVerde);
+		cp.add(radiocol); 
+		cp.setBackground(Color.WHITE);
 
 		clases.add(cp);
 //
@@ -69,6 +70,7 @@ public class PanelRegistro2 extends Panel {
 		ContentPane cp1 = new ContentPane();
 		cp1.setLayoutData(acc1);
 		cp1.add(btnRadioAmarillo);
+		cp1.setBackground(Color.WHITE);
 
 		clases.add(cp1);
 	
@@ -80,6 +82,7 @@ public class PanelRegistro2 extends Panel {
 		ContentPane cp2 = new ContentPane();
 		cp2.setLayoutData(acc2);
 		cp2.add(btnRadioNegro);
+		cp2.setBackground(Color.WHITE);
 
 		clases.add(cp2);		
 
@@ -91,10 +94,11 @@ public class PanelRegistro2 extends Panel {
 		ContentPane cp3 = new ContentPane();
 		cp3.setLayoutData(acc3);
 		cp3.add(btnRadioRojo);
+		cp3.setBackground(Color.WHITE);
 
 		clases.add(cp3);
-
-
+//
+//
 		RadioButton btnRadioAzul = new RadioButton("Azul - Blue Lantern Corps");
 //		btnGroupClases.addButton(btnRadioAzul);
 		AccordionPaneLayoutData acc4 = new AccordionPaneLayoutData();
@@ -102,31 +106,37 @@ public class PanelRegistro2 extends Panel {
 		ContentPane cp4 = new ContentPane();
 		cp4.setLayoutData(acc4);
 		cp4.add(btnRadioAzul);
+		cp4.setBackground(Color.WHITE);
 
-		clases.add(cp4);
+		clases.add(cp4); //TODO: no funciona con mas de 5 tabs
 		
 		
-		RadioButton btnRadioIndigo = new RadioButton("Indigo - La Tribu Indigo");
-//		btnGroupClases.addButton(btnRadioIndigo);
-		AccordionPaneLayoutData acc5 = new AccordionPaneLayoutData();
-		acc5.setTitle("Indigo - La Tribu Indigo");
-		ContentPane cp5 = new ContentPane();
-		cp5.setLayoutData(acc5);
-		cp5.set(PROPERTY_BACKGROUND, Color.LIGHTGRAY);
-		cp5.add(btnRadioIndigo);
-
-		clases.add(cp5);
-		
-		RadioButton btnRadioVioleta = new RadioButton("Violeta - Star Sapphires");
-//		btnGroupClases.addButton(btnRadioVioleta);
-		AccordionPaneLayoutData acc6 = new AccordionPaneLayoutData();
-		acc6.setTitle("Violeta - Star Sapphires");
-		ContentPane cp6 = new ContentPane();
-		cp6.setLayoutData(acc6);
-		cp6.set(PROPERTY_BACKGROUND, Color.LIGHTGRAY);
-		cp6.add(btnRadioVioleta);
-
-		clases.add(cp6);
+//		RadioButton btnRadioIndigo = new RadioButton("Indigo - La Tribu Indigo");
+////		btnGroupClases.addButton(btnRadioIndigo);
+//		AccordionPaneLayoutData acc5 = new AccordionPaneLayoutData();
+//		acc5.setTitle("Indigo - La Tribu Indigo");
+//		ContentPane cp5 = new ContentPane();
+//		cp5.setLayoutData(acc5);
+//		//cp5.set(PROPERTY_BACKGROUND, Color.LIGHTGRAY);
+//		cp5.add(btnRadioIndigo);
+//		cp5.setBackground(Color.WHITE);
+//
+//		clases.add(cp5);
+//		
+//		RadioButton btnRadioVioleta = new RadioButton("Violeta - Star Sapphires");
+////		btnGroupClases.addButton(btnRadioVioleta);
+//		AccordionPaneLayoutData acc6 = new AccordionPaneLayoutData();
+//		acc6.setTitle("Violeta - Star Sapphires");
+//		ContentPane cp6 = new ContentPane();
+//		cp6.setLayoutData(acc6);
+//		//cp6.set(PROPERTY_BACKGROUND, Color.LIGHTGRAY);
+//		cp6.add(btnRadioVioleta);
+//
+//		clases.add(cp6);
+//		
+		//clases.setActiveTabIndex(0);
+		clases.setAnimationTime(300);
+		clases.set(PROPERTY_BORDER, Border.STYLE_SOLID);
 		
 		//grid.add(btnGroupClases);
 
