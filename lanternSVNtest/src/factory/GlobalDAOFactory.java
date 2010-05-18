b68baeb53c0429da768610fed0e3ded34c2a5aad
josed43 <josed43@6532114f-c851-e024-fd2a-a63d0af6f600>
2010-05-18 08:45:52 +0000

    Fabrica completa

--- lanternSVNtest/src/factory/GlobalDAOFactory.java
+++ lanternSVNtest/src/factory/GlobalDAOFactory.java
@@ -25,12 +25,12 @@ public class GlobalDAOFactory {
 		LCA, LANTERN
 	}
 
-	//----------------------------------------------------------------------
-	
+	// ----------------------------------------------------------------------
+
 	private static final Grupo GRUPO = Grupo.LCA;
 
-	//----------------------------------------------------------------------
-	
+	// ----------------------------------------------------------------------
+
 	public static InterfaceDAO getDAO(Class<? extends InterfaceDAO> clazz,
 			ConnectionBean connectionBean) throws Exception {
 		// clazz -> IXXXDAO
@@ -45,144 +45,169 @@ public class GlobalDAOFactory {
 		throw new IllegalStateException();
 	}
 
-	//----------------------------------------------------------------------
-	
-	private static InterfaceDAO createDAOLCA(Class<? extends InterfaceDAO> clazz, ConnectionBean connectionBean)
+	// ----------------------------------------------------------------------
+
+	private static InterfaceDAO createDAOLCA(
+			Class<? extends InterfaceDAO> clazz, ConnectionBean connectionBean)
 			throws Exception {
-		
+
 		if (clazz.equals(IUsuarioDAO.class)) {
-			
+
 			return FactoryDAO.getDAO(dao.lca.UsuarioDAO.class, connectionBean);
-			
+
 		} else if (clazz.equals(IPlanetaDAO.class)) {
-			
+
 			return FactoryDAO.getDAO(dao.lca.PlanetaDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IPersonajeDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.PersonajeDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IOrdenDAO.class)) {
-			
+
+		} else if (clazz.equals(IPersonajeDAO.class)) {
+
+			return FactoryDAO
+					.getDAO(dao.lca.PersonajeDAO.class, connectionBean);
+
+		} else if (clazz.equals(IOrdenDAO.class)) {
+
 			return FactoryDAO.getDAO(dao.lca.OrdenDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IObjetivoDAO.class)) {
-			
+
+		} else if (clazz.equals(IObjetivoDAO.class)) {
+
 			return FactoryDAO.getDAO(dao.lca.ObjetivoDAO.class, connectionBean);
-			
-		}else if (clazz.equals(INpcDAO.class)) {
-			
+
+		} else if (clazz.equals(INpcDAO.class)) {
+
 			return FactoryDAO.getDAO(dao.lca.NpcDAO.class, connectionBean);
-			
-		}else if (clazz.equals(INivelHabilidadDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.NivelHabilidadDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IMisionPersonajeDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.MisionPersonajeDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IMisionDAO.class)) {
-			
+
+		} else if (clazz.equals(INivelHabilidadDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lca.NivelHabilidadDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IMisionPersonajeDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lca.MisionPersonajeDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IMisionDAO.class)) {
+
 			return FactoryDAO.getDAO(dao.lca.MisionDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IMisionClaseLinternaDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.MisionClaseLinternaDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IHabilidadDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.HabilidadDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IHabilidadClaseLinternaDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.HabilidadClaseLinternaDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IHabilidadActivaDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.HabilidadActivaDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IGrupoDAO.class)) {
-			
+
+		} else if (clazz.equals(IMisionClaseLinternaDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lca.MisionClaseLinternaDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IHabilidadDAO.class)) {
+
+			return FactoryDAO
+					.getDAO(dao.lca.HabilidadDAO.class, connectionBean);
+
+		} else if (clazz.equals(IHabilidadClaseLinternaDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lca.HabilidadClaseLinternaDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IHabilidadActivaDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lca.HabilidadActivaDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IGrupoDAO.class)) {
+
 			return FactoryDAO.getDAO(dao.lca.GrupoDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IClaseLinternaDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.ClaseLinternaDAO.class, connectionBean);
-			
+
+		} else if (clazz.equals(IClaseLinternaDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lca.ClaseLinternaDAO.class,
+					connectionBean);
+
 		}
 
 		throw new IllegalStateException();
 	}
 
-	//----------------------------------------------------------------------
-	
-	private static InterfaceDAO createDAOLANTERN(Class<? extends InterfaceDAO> clazz, ConnectionBean connectionBean)
+	// ----------------------------------------------------------------------
+
+	private static InterfaceDAO createDAOLANTERN(
+			Class<? extends InterfaceDAO> clazz, ConnectionBean connectionBean)
 			throws Exception {
 
 		if (clazz.equals(IUsuarioDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.UsuarioDAO.class, connectionBean);
-			
+
+			return FactoryDAO.getDAO(dao.lantern.UsuarioDAO.class,
+					connectionBean);
+
 		} else if (clazz.equals(IPlanetaDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.PlanetaDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IPersonajeDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.PersonajeDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IOrdenDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.OrdenDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IObjetivoDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.ObjetivoDAO.class, connectionBean);
-			
-		}else if (clazz.equals(INpcDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.NpcDAO.class, connectionBean);
-			
-		}else if (clazz.equals(INivelHabilidadDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.NivelHabilidadDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IMisionPersonajeDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.MisionPersonajeDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IMisionDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.MisionDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IMisionClaseLinternaDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.MisionClaseLinternaDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IHabilidadDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.HabilidadDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IHabilidadClaseLinternaDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.HabilidadClaseLinternaDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IHabilidadActivaDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.HabilidadActivaDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IGrupoDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.GrupoDAO.class, connectionBean);
-			
-		}else if (clazz.equals(IClaseLinternaDAO.class)) {
-			
-			return FactoryDAO.getDAO(dao.lca.ClaseLinternaDAO.class, connectionBean);
-			
+
+			return FactoryDAO.getDAO(dao.lantern.PlanetaDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IPersonajeDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.PersonajeDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IOrdenDAO.class)) {
+
+			return FactoryDAO
+					.getDAO(dao.lantern.OrdenDAO.class, connectionBean);
+
+		} else if (clazz.equals(IObjetivoDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.ObjetivoDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(INpcDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.NpcDAO.class, connectionBean);
+
+		} else if (clazz.equals(INivelHabilidadDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.NivelHabilidadDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IMisionPersonajeDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.MisionPersonajeDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IMisionDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.MisionDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IMisionClaseLinternaDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.MisionClaseLinternaDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IHabilidadDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.HabilidadDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IHabilidadClaseLinternaDAO.class)) {
+
+			return FactoryDAO
+					.getDAO(dao.lantern.HabilidadClaseLinternaDAO.class,
+							connectionBean);
+
+		} else if (clazz.equals(IHabilidadActivaDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.HabilidadActivaDAO.class,
+					connectionBean);
+
+		} else if (clazz.equals(IGrupoDAO.class)) {
+
+			return FactoryDAO
+					.getDAO(dao.lantern.GrupoDAO.class, connectionBean);
+
+		} else if (clazz.equals(IClaseLinternaDAO.class)) {
+
+			return FactoryDAO.getDAO(dao.lantern.ClaseLinternaDAO.class,
+					connectionBean);
+
 		}
 
 		throw new IllegalStateException();
 	}
-	
+
 }
--- lanternSVNtest/src/factory/GlobalDOFactory.java
+++ lanternSVNtest/src/factory/GlobalDOFactory.java
@@ -1,24 +1,37 @@
 package factory;
 
+import lcaInterfaceDAO.IClaseLinternaDO;
+import lcaInterfaceDAO.IGrupoDO;
+import lcaInterfaceDAO.IHabilidadActivaDO;
+import lcaInterfaceDAO.IHabilidadClaseLinternaDO;
+import lcaInterfaceDAO.IHabilidadDO;
+import lcaInterfaceDAO.IMisionClaseLinternaDO;
+import lcaInterfaceDAO.IMisionDO;
+import lcaInterfaceDAO.IMisionPersonajeDO;
+import lcaInterfaceDAO.INivelHabilidadDO;
+import lcaInterfaceDAO.INpcDO;
+import lcaInterfaceDAO.IObjetivoDO;
+import lcaInterfaceDAO.IOrdenDO;
+import lcaInterfaceDAO.IPersonajeDO;
+import lcaInterfaceDAO.IPlanetaDO;
 import lcaInterfaceDAO.IUsuarioDO;
 import dao.api.DataObject;
 
 public class GlobalDOFactory {
-	
+
 	public enum Grupo {
 		LCA, LANTERN
 	}
-	
-	//----------------------------------------------------------------------
-	
+
+	// ----------------------------------------------------------------------
+
 	private static final Grupo GRUPO = Grupo.LCA;
-	
-	//----------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------
 
 	public static DataObject getDO(Class<? extends DataObject> clazz)
 			throws Exception {
 		// clazz -> IXXXDO
-
 		switch (GRUPO) {
 		case LCA:
 			return createDOLCA(clazz);
@@ -28,26 +41,139 @@ public class GlobalDOFactory {
 
 		throw new IllegalStateException();
 	}
-	
-	//----------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------
 
 	private static DataObject createDOLCA(Class<? extends DataObject> clazz) {
+
 		if (clazz.equals(IUsuarioDO.class)) {
+
 			return new dao.lca.UsuarioDO();
-		} else if (true) {
-			// ....
+
+		} else if (clazz.equals(IPlanetaDO.class)) {
+
+			return new dao.lca.PlanetaDO();
+
+		} else if (clazz.equals(IPersonajeDO.class)) {
+
+			return new dao.lca.PersonajeDO();
+
+		} else if (clazz.equals(IOrdenDO.class)) {
+
+			return new dao.lca.OrdenDO();
+
+		} else if (clazz.equals(IObjetivoDO.class)) {
+
+			return new dao.lca.ObjetivoDO();
+
+		} else if (clazz.equals(INpcDO.class)) {
+
+			return new dao.lca.NpcDO();
+
+		} else if (clazz.equals(INivelHabilidadDO.class)) {
+
+			return new dao.lca.NivelHabilidadDO();
+
+		} else if (clazz.equals(IMisionPersonajeDO.class)) {
+
+			return new dao.lca.MisionPersonajeDO();
+
+		} else if (clazz.equals(IMisionDO.class)) {
+
+			return new dao.lca.MisionDO();
+
+		} else if (clazz.equals(IMisionClaseLinternaDO.class)) {
+
+			return new dao.lca.MisionClaseLinternaDO();
+
+		} else if (clazz.equals(IHabilidadDO.class)) {
+
+			return new dao.lca.HabilidadDO();
+
+		} else if (clazz.equals(IHabilidadClaseLinternaDO.class)) {
+
+			return new dao.lca.HabilidadClaseLinternaDO();
+
+		} else if (clazz.equals(IHabilidadActivaDO.class)) {
+
+			return new dao.lca.HabilidadActivaDO();
+
+		} else if (clazz.equals(IGrupoDO.class)) {
+
+			return new dao.lca.GrupoDO();
+
+		} else if (clazz.equals(IClaseLinternaDO.class)) {
+
+			return new dao.lca.ClaseLinternaDO();
+
 		}
 
 		throw new IllegalStateException();
 	}
-	
-	//----------------------------------------------------------------------
+
+	// ----------------------------------------------------------------------
 
 	private static DataObject createDOLANTERN(Class<? extends DataObject> clazz) {
 		if (clazz.equals(IUsuarioDO.class)) {
-			return new dao.lca.UsuarioDO();
-		} else if (true) {
-			// ....
+
+			return new dao.lantern.UsuarioDO();
+
+		} else if (clazz.equals(IPlanetaDO.class)) {
+
+			return new dao.lantern.PlanetaDO();
+
+		} else if (clazz.equals(IPersonajeDO.class)) {
+
+			return new dao.lantern.PersonajeDO();
+
+		} else if (clazz.equals(IOrdenDO.class)) {
+
+			return new dao.lantern.OrdenDO();
+
+		} else if (clazz.equals(IObjetivoDO.class)) {
+
+			return new dao.lantern.ObjetivoDO();
+
+		} else if (clazz.equals(INpcDO.class)) {
+
+			return new dao.lantern.NpcDO();
+
+		} else if (clazz.equals(INivelHabilidadDO.class)) {
+
+			return new dao.lantern.NivelHabilidadDO();
+
+		} else if (clazz.equals(IMisionPersonajeDO.class)) {
+
+			return new dao.lantern.MisionPersonajeDO();
+
+		} else if (clazz.equals(IMisionDO.class)) {
+
+			return new dao.lantern.MisionDO();
+
+		} else if (clazz.equals(IMisionClaseLinternaDO.class)) {
+
+			return new dao.lantern.MisionClaseLinternaDO();
+
+		} else if (clazz.equals(IHabilidadDO.class)) {
+
+			return new dao.lantern.HabilidadDO();
+
+		} else if (clazz.equals(IHabilidadClaseLinternaDO.class)) {
+
+			return new dao.lantern.HabilidadClaseLinternaDO();
+
+		} else if (clazz.equals(IHabilidadActivaDO.class)) {
+
+			return new dao.lantern.HabilidadActivaDO();
+
+		} else if (clazz.equals(IGrupoDO.class)) {
+
+			return new dao.lantern.GrupoDO();
+
+		} else if (clazz.equals(IClaseLinternaDO.class)) {
+
+			return new dao.lantern.ClaseLinternaDO();
+
 		}
 
 		throw new IllegalStateException();
