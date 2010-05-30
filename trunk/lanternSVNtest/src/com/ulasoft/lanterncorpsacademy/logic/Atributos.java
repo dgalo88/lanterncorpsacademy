6e7e1853933f8e64bfa33c7506b367271c7427e7
knighthash@gmail.com <knighthash@gmail.com@6532114f-c851-e024-fd2a-a63d0af6f600>
2010-05-30 20:20:44 +0000

    IS completa la lógica del login, y del logout..

--- trunk/lanternSVNtest/src/com/ulasoft/lanterncorpsacademy/logic/Atributos.java
+++ trunk/lanternSVNtest/src/com/ulasoft/lanterncorpsacademy/logic/Atributos.java
@@ -1,11 +1,15 @@
 package com.ulasoft.lanterncorpsacademy.logic;
 
+import lcaInterfaceDAO.IMisionPersonajeDAO;
 import lcaInterfaceDAO.IPersonajeDAO;
 import lcaInterfaceDAO.IPersonajeDO;
+import lcaInterfaceDAO.IPlanetaDAO;
+import lcaInterfaceDAO.IPlanetaDO;
 import lcaInterfaceDAO.IUsuarioDAO;
 import lcaInterfaceDAO.IUsuarioDO;
 
 import com.ulasoft.lanterncorpsacademy.menus.Menud;
+import com.ulasoft.lanterncorpsacademy.paneles.PanelMain;
 
 import dao.connection.ConnectionBean;
 import dao.connection.ConnectionFactory;
@@ -38,22 +42,73 @@ public class Atributos {
 	
 	public void updateMenud(Menud menud) throws Exception {
 		
-		ConnectionBean connectionBean=ConnectionFactory.getConnectionBean();
-		IPersonajeDAO personajeDAO = (IPersonajeDAO) GlobalDAOFactory.getDAO(IPersonajeDAO.class, connectionBean);
-		//personaje = (IPersonajeDO) GlobalDOFactory.getDO(IPersonajeDO.class);
+//		ConnectionBean connectionBean=ConnectionFactory.getConnectionBean();
+//		IPersonajeDAO personajeDAO = (IPersonajeDAO) GlobalDAOFactory.getDAO(IPersonajeDAO.class, connectionBean);
+//		//personaje = (IPersonajeDO) GlobalDOFactory.getDO(IPersonajeDO.class);
+//		
+		System.err.println("PERSONAJE ID en atts menud:" + personaje.getId());
+		System.err.println("PERSONAJE salud en atts menud:"
+				+ personaje.getSalud());
+		// personaje= (IPersonajeDO) personajeDAO.loadById(personaje.getId());
+		menud.getSalud().setCurrValue(
+				(personaje.getSalud()) * 100
+						/ (200 + 50 * (personaje.getNivel() - 1)));
+		menud.getEnergia().setCurrValue(
+				personaje.getEnergiaDelAnillo() * 100
+						/ (100 + 10 * (personaje.getNivel() - 1)));
+		menud.getExperiencia().setCurrValue(
+				personaje.getExperiencia() * 100 / 15 * 
+				 2^(personaje.getNivel() - 1));
+		menud.getLblTrainsValue().setText(
+				Integer.toString(personaje.getPuntosDeEntrenamiento()));
+		menud.getLblNiveLabelValue().setText(
+				Integer.toString(personaje.getNivel()));
 		
-		System.err.println("PERSONAJE ID en atts:"+personaje.getId());
-		personaje= (IPersonajeDO) personajeDAO.loadById(personaje.getId());
-		menud.getSalud().setCurrValue(personaje.getSalud());
-		menud.getEnergia().setCurrValue(personaje.getEnergiaDelAnillo());
-		menud.getExperiencia().setCurrValue(personaje.getExperiencia());
-		menud.getLblTrainsValue().setText(Integer.toString(personaje.getPuntosDeEntrenamiento()));
-		menud.getLblNiveLabelValue().setText(Integer.toString(personaje.getNivel()));
-		
-		ConnectionFactory.closeConnection(connectionBean.getConnection());
+//		ConnectionFactory.closeConnection(connectionBean.getConnection());
+		System.err.println("PERSONAJE ID en atts menud2:" + personaje.getId());
 		
 	}
 
+	public void updatePanelMain(PanelMain main) throws Exception {
+		
+		ConnectionBean connectionBean=ConnectionFactory.getConnectionBean();
+		IPlanetaDAO planetaDAO = (IPlanetaDAO) GlobalDAOFactory.getDAO(IPlanetaDAO.class, connectionBean);
+		//IPlanetaDO planeta = (IPlanetaDO) GlobalDOFactory.getDO(IPlanetaDO.class);
+		IMisionPersonajeDAO misionPersonDAO = (IMisionPersonajeDAO) GlobalDAOFactory
+				.getDAO(IMisionPersonajeDAO.class, connectionBean);
+		
+		IPlanetaDO planeta= (IPlanetaDO) planetaDAO.loadById(personaje.getPlanetaRef().getRefIdent());
+		int misionCount= misionPersonDAO.countByPersonajeId(personaje.getId());
+		ConnectionFactory.closeConnection(connectionBean.getConnection());
+		
+		System.err.println("PLANETA ID en atts main:" + planeta.getId());
+		main.getLblPlanetaValue().setText(planeta.getNombre());
+		main.getLblSectorValue().setText(planeta.getSector());
+		main.getLblFechaValue().setText(personaje.getUltimaFechaIngreso().toString());
+		main.getLblMisionesValue().setText(Integer.toString(misionCount));
+		
+	}
+
+public void guardarAtts() throws Exception {
+	ConnectionBean connectionBean=ConnectionFactory.getConnectionBean();
+	    
+	IPersonajeDAO personajeDAO = (IPersonajeDAO) GlobalDAOFactory.getDAO(IPersonajeDAO.class, connectionBean);
+//    IUsuarioDAO usDAO = (IUsuarioDAO) GlobalDAOFactory.getDAO(IUsuarioDAO.class, connectionBean);
+//    usuario = (IUsuarioDO) usDAO.loadById(usuario.getId());
+    personajeDAO.update(personaje);
+	
+	System.err.println("PERSONAJE ID en atts save:"+personaje.getId());
+	
+	ConnectionFactory.closeConnection(connectionBean.getConnection());
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 	public IPersonajeDO getPersonaje() {
 		return personaje;
 	}
@@ -69,5 +124,9 @@ public class Atributos {
 	public void setUsuario(IUsuarioDO usuario) {
 		this.usuario = usuario;
 	}
+
+	
+
+
 
 }
