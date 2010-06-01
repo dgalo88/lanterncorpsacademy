a62077127e65c274a3c7a563f7467b9a50cb1edd
styp152 <styp152@6532114f-c851-e024-fd2a-a63d0af6f600>
2010-06-01 20:41:32 +0000

    Arreglo del Panel Foro para la Presentacion

--- trunk/lanternSVNtest/src/com/ulasoft/lanterncorpsacademy/paneles/PanelForo.java
+++ trunk/lanternSVNtest/src/com/ulasoft/lanterncorpsacademy/paneles/PanelForo.java
@@ -1,12 +1,38 @@
 package com.ulasoft.lanterncorpsacademy.paneles;
 
+import nextapp.echo.app.Alignment;
+import nextapp.echo.app.Border;
+import nextapp.echo.app.Color;
+import nextapp.echo.app.Column;
+import nextapp.echo.app.Extent;
 import nextapp.echo.app.Label;
 import nextapp.echo.app.Panel;
+import nextapp.echo.app.ResourceImageReference;
+import nextapp.echo.app.Row;
 
-public class PanelForo extends Panel{
+public class PanelForo extends Panel {
 
 	public PanelForo() {
-		Label lblMain = new Label("Foro");
-	    add(lblMain);
+		Column col = new Column();
+		
+		Row row = new Row();
+		Label lblMain = new Label("PROXIMAMENTE");
+		lblMain.set(PROPERTY_FONT, new Extent(100));
+		lblMain.setBackground(Color.WHITE);
+		row.add(lblMain);
+		row.set(PROPERTY_WIDTH, new Extent(950));
+		row.setAlignment(Alignment.ALIGN_CENTER);
+		
+		col.add(row);
+		
+		row = new Row();
+		Label lblImagen = new Label();
+		lblImagen.setIcon(new ResourceImageReference(//
+				"com/ulasoft/lanterncorpsacademy/imagenes/glcbanner.gif"));
+		row.add(lblImagen);
+		row.setAlignment(Alignment.ALIGN_CENTER);
+		col.add(row);
+		add(col);
+		
 	}
 }
