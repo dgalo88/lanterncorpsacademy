e3e0876c44157bdf2a9851a9870d143d2c760927
knighthash <knighthash@gmail.com>
2010-06-02 09:36:34 +0000

    BD1 habilidad dao con la consulta nueva

--- BDLanternCorps1/src/dao/lantern/HabilidadDAO.java
+++ BDLanternCorps1/src/dao/lantern/HabilidadDAO.java
@@ -237,7 +237,7 @@ public class HabilidadDAO extends BaseDAO implements IHabilidadDAO{
 		strbuf.append(habilidadClaseLinternaDAO.getTableName());
 		strbuf.append("." + HabilidadClaseLinternaDO.HABILIDAD_ID);
 
-		strbuf.append("RIGHT JOIN  ");
+		strbuf.append("  RIGHT JOIN  ");
 		strbuf.append(habilidadActivaDAO.getTableName());
 		strbuf.append(" ON ");
 		strbuf.append(personajeDAO.getTableName());
