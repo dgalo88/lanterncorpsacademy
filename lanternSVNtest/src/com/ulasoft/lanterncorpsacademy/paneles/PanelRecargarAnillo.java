6145b1d3e910a41368fa40cefc51b740cc3ef3bd
styp152 <styp152@6532114f-c851-e024-fd2a-a63d0af6f600>
2010-06-02 00:14:00 +0000

    Logica y Presentacion arreglados para cargar la imagen de Recarga del Anillo segun la clase del Usuario, ademas detalles de Presentacion en el Panel Foro

--- lanternSVNtest/src/com/ulasoft/lanterncorpsacademy/paneles/PanelRecargarAnillo.java
+++ lanternSVNtest/src/com/ulasoft/lanterncorpsacademy/paneles/PanelRecargarAnillo.java
@@ -1,10 +1,5 @@
 package com.ulasoft.lanterncorpsacademy.paneles;
 
-import com.ulasoft.lanterncorpsacademy.Desktop;
-import com.ulasoft.lanterncorpsacademy.GUIStyles;
-import com.ulasoft.lanterncorpsacademy.LanternCorpsAcademyApp;
-import com.ulasoft.lanterncorpsacademy.logic.Atributos;
-
 import nextapp.echo.app.Alignment;
 import nextapp.echo.app.Button;
 import nextapp.echo.app.Color;
@@ -18,71 +13,85 @@ import nextapp.echo.app.Row;
 import nextapp.echo.app.event.ActionEvent;
 import nextapp.echo.app.event.ActionListener;
 
-public class PanelRecargarAnillo extends Panel{
+import com.ulasoft.lanterncorpsacademy.Desktop;
+import com.ulasoft.lanterncorpsacademy.GUIStyles;
+import com.ulasoft.lanterncorpsacademy.LanternCorpsAcademyApp;
+import com.ulasoft.lanterncorpsacademy.logic.Atributos;
+import com.ulasoft.lanterncorpsacademy.logic.ImgLoad;
 
+public class PanelRecargarAnillo extends Panel {
+
+	LanternCorpsAcademyApp lca = (LanternCorpsAcademyApp) LanternCorpsAcademyApp
+	.getActive();
+	
 	public PanelRecargarAnillo() {
 		Column col = new Column();
-		col.setInsets(new Insets(2,2,2,2));
-		
-		col.add(new Label("Recarga de Anillo"));
-		
-		Label lblImagen = new Label(); 
-		lblImagen.setIcon(new ResourceImageReference("com/ulasoft/lanterncorpsacademy/imagenes/jim-lee-green-lantern.jpg",new Extent(152), new Extent(232)));
+		col.setInsets(new Insets(2, 2, 2, 2));
+		Label lblTitle = new Label("Recarga de Anillo");
+		lblTitle.setBackground(Color.WHITE);
+		col.add(lblTitle);
+
+		Label lblImagen = new Label();
+		lblImagen
+				.setIcon(new ResourceImageReference(ImgLoad.panelRecarga(lca
+								.getAtributos().getPersonaje()),
+						new Extent(200), new Extent(300)));
 		col.add(lblImagen);
 		
-		col.add(new Label("Nivel	xx"));
+		Label lblNivel = new Label("Nivel "+ lca.getAtributos().getPersonaje().getNivel());
+		lblNivel.setBackground(Color.WHITE);
+		col.add(lblNivel);
 		
-		Row row = new Row(); 
-		
-//		Button btnCargarBatPrin = new Button("Cargar con Bateria Central");
-//		btnCargarBatPrin.setStyle(GUIStyles.DEFAULT_STYLE);
-//		btnCargarBatPrin.addActionListener(new ActionListener() {
-//	      @Override
-//	      public void actionPerformed(ActionEvent arg0) {
-//	    	  btnCargarBatPrinClicked();
-//	      }
-//	    });
-//	    row.add(btnCargarBatPrin);
-		
-	    Button btnCargarBatPort = new Button("Cargar con Bateria Portatil");
-	    btnCargarBatPort.setStyle(GUIStyles.DEFAULT_STYLE);
-	    btnCargarBatPort.setWidth(new Extent(300))
-;	    btnCargarBatPort.addActionListener(new ActionListener() {
-	      @Override
-	      public void actionPerformed(ActionEvent arg0) {
-	    	  btnCargarBatPortClicked();
-	      }
-	    });
-	    row.add(btnCargarBatPort);
-	    row.setAlignment(Alignment.ALIGN_CENTER);
-	    col.add(row);
-	    col.setBackground(Color.WHITE);
-	    this.add(col);
+		Row row = new Row();
+
+		// Button btnCargarBatPrin = new Button("Cargar con Bateria Central");
+		// btnCargarBatPrin.setStyle(GUIStyles.DEFAULT_STYLE);
+		// btnCargarBatPrin.addActionListener(new ActionListener() {
+		// @Override
+		// public void actionPerformed(ActionEvent arg0) {
+		// btnCargarBatPrinClicked();
+		// }
+		// });
+		// row.add(btnCargarBatPrin);
+
+		Button btnCargarBatPort = new Button("Cargar con Bateria Portatil");
+		btnCargarBatPort.setStyle(GUIStyles.DEFAULT_STYLE);
+		btnCargarBatPort.setWidth(new Extent(300));
+		btnCargarBatPort.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				btnCargarBatPortClicked();
+			}
+		});
+		row.add(btnCargarBatPort);
+		row.setAlignment(Alignment.ALIGN_CENTER);
+		col.add(row);
+		this.add(col);
 	}
 
-	LanternCorpsAcademyApp lca = (LanternCorpsAcademyApp) LanternCorpsAcademyApp.getActive();
+	
 	Atributos atrib = lca.getAtributos();
 	Desktop d = lca.getDesktop();
-	
+
 	protected void btnCargarBatPortClicked() {
-		
+
 		try {
 			atrib.recargaAnillo();
-			
+
 		} catch (Exception e) {
 
 			e.printStackTrace();
 		}
-		
+
 	}
 
-//	protected void btnCargarBatPrinClicked() {
-//		try {
-//			atrib.recargaAnillo();
-//		} catch (Exception e) {
-//
-//			e.printStackTrace();
-//		}
-//		
-//	}
+	// protected void btnCargarBatPrinClicked() {
+	// try {
+	// atrib.recargaAnillo();
+	// } catch (Exception e) {
+	//
+	// e.printStackTrace();
+	// }
+	//		
+	// }
 }
